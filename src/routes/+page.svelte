d067078d40e36793cec288ccaa913478c2b9b0c1
HelluvaBird <royceman24@hotmail.com>
2024-02-03 17:13:13 -0500

    update the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,13 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
+		<h1 class="h1">Let's get cracking tickets!</h1>
 		<p>Start by exploring:</p>
 		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
+			<li><code class="code">Login to the site</code> - Logging in allows you to create tickets</li>
+			<li><code class="code">Create a ticket</code> - If you have a problem, we're here to help</li>
 			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+				<code class="code">View all tickets</code> - View all newly created, open, and closed tickets
 			</li>
 		</ul>
 	</div>
